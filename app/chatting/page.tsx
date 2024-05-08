8e6a0dd542ed7dfece1c1f1c1b77628d46230b92
Anirudh Singh Chouhan <140189477+AnirudhSingh07@users.noreply.github.com>
2024-05-09 01:55:25 +0530

    minor changes

---
+++ app/chatting/page.tsx
@@ -0,0 +1,12 @@
+import { Chatting } from '@/components/component/chatting'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Chatting />
+    </div>
+  )
+}
+
+export default page
