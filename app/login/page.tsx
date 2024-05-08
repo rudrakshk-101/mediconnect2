7526326df93d701c6fd712db04e328542517c104
Anirudh Singh Chouhan <140189477+AnirudhSingh07@users.noreply.github.com>
2024-05-08 16:25:09 +0530

    login page

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '@/components/component/login'
+
+const page = () => {
+  return (
+    <div>
+      <Login />  
+    </div>
+  )
+}
+
+export default page
