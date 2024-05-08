39cc5c320ba92fed9389d0a7665ad1e079e1974f
Anirudh Singh Chouhan <140189477+AnirudhSingh07@users.noreply.github.com>
2024-05-09 01:51:50 +0530

    monitoring and dashboard

---
+++ app/patient-dashboard/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+import Monitoring from '@/components/component/monitoring'
+
+const page = () => {
+  return (
+    <div>
+      <Monitoring />  
+    </div>
+  )
+}
+
+export default page
+
