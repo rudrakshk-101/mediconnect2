064a6a94a060a51ecfcd49461207295c126a7dde
Anirudh Singh Chouhan <140189477+AnirudhSingh07@users.noreply.github.com>
2024-05-10 13:33:19 +0530

    minor changes

---
+++ app/hospitalhome/page.tsx
@@ -0,0 +1,12 @@
+import { HospitalHomePage } from '@/components/component/hospitalhome-page';
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <HospitalHomePage />
+    </div>
+  )
+}
+
+export default page;
