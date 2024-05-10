bcb2d6bcdf2ca4e3b52debd3a2b82a51c8261df5
Anirudh Singh Chouhan <140189477+AnirudhSingh07@users.noreply.github.com>
2024-05-10 16:19:41 +0530

    insurance

---
+++ app/insurance/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Insurance from '@/components/component/insurance'
+const page = () => {
+  return (
+    <div>
+        <Insurance />
+      
+    </div>
+  )
+}
+
+export default page
